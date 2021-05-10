25c00c4fe3725c64f591175609d23993f74f893a
Positron11 <aarushbeleren11@gmail.com>
2021-05-10 14:40:16 +0530

    DO THING ONLY WHEN DOUBLECLICK

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,7 +1,7 @@
 var audio = document.getElementById("why_run_audio");
 
 $(function(){
-	$("#oxygen_coefficient").on("click", function() {
+	$("#oxygen_coefficient").on("dblclick", function() {
 		$("#official_tagline").toggle(); 
 		$("#real_tagline").toggle();
 		$("#real_tagline").is(":visible") ? audio.play() : audio.pause();
